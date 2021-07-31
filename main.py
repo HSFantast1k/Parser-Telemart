d0ac13f3354c4d7903691860be8c7f6c15de8cf8
HSFantast1k <83510907+HSFantast1k@users.noreply.github.com>
2021-07-31 21:50:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,24 +7,29 @@ base_list = []
 def parser_of_goods(link_product):
     response = requests.get(link_product, headers=HEADERS)
     soup = BeautifulSoup(response.content, 'html.parser')
+    print('Название: {}'.format(soup.find('h1', 'b-page-title').get_text()))
     base_list.append([
-        'Название - {}'.format(soup.find('h1', 'b-page-title').get_text()),
-        'Цена - {}'.format(soup.find('div', class_='b-price').get_text()),
-        'Наличие - {}'.format('None')
+        'Название: {}'.format(soup.find('h1', 'b-page-title').get_text()),
+        'Цена: {}'.format(None),
+        'Наличие: {}'.format(None)
     ])
-    if soup.find('div', 'b-i-product-available b-i-product-available-green').get_text() == 'Есть в наличии':
-        base_list[-1][-1] = 'Наличие {}'.format(True)
-    else:
-        base_list[-1][-1] = 'Наличие {}'.format(False)
-    print(base_list)
+    try:
+        soup.find('div', 'b-i-product-available b-i-product-available-green').get_text()
+        base_list[-1][-1] = 'Наличие: {}'.format(True)
+        base_list[-1][-2] = 'Цена: {}'.format(
+        soup.find('div', class_='b-pmi-col-left').find('div', class_="b-price").get_text())
+    except:
+        base_list[-1][-1] = 'Наличие: {}'.format(False)
+        base_list[-1][-2] = 'Цена: {}'.format(None)
+
 
 def parser_page():
     page = 1
     while True:
         if page == 1:
-            response = requests.get('https://telemart.ua/laptops/', headers=HEADERS)
+            response = requests.get('https://telemart.ua/processor/filter/amd/intel/', headers=HEADERS)
         else:
-            response = requests.get('https://telemart.ua/laptops/p{}/'.format(page), headers=HEADERS)
+            response = requests.get(f'https://telemart.ua/processor/filter/amd/intel/p{page}/', headers=HEADERS)
         soup = BeautifulSoup(response.content, 'html.parser')
         items = soup.find_all('div', class_='b-i-product-inner')
         if len(items):
@@ -34,6 +39,8 @@ def parser_page():
                 parser_of_goods(item.find('div', 'b-i-product-name').find('a').get('href'))
             page += 1
         else:
+            for ba_list in base_list:
+                print(' '.join([i for i in ba_list]))
             return
 
 parser_page()
